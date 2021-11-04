d603655f273a3bdcbfe153d83e0de33da70d3eee
docongban <banto2172001@gmail.com>
2021-11-04 23:40:34 +0700

    add slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,51 @@
+//Slider
+const slider=document.querySelector('.slider')
+const sliderMain = document.querySelector('.slider-main')
+const sliderItems = document.querySelectorAll('.slider-item')
+const nextBtn = document.querySelector('.slider-right')
+const prevBtn = document.querySelector('.slider-left')
+const sliderItemWidth = sliderItems[0].offsetWidth
+const numberOfSliderItems = sliderItems.length 
+
+var slideNumber = 0
+
+//Next Slider
+nextBtn.addEventListener('click',() => {
+  slideNumber++;
+  
+  if(slideNumber > (numberOfSliderItems-1) ){
+      slideNumber = 0;
+  }
+
+  sliderMain.style.left = "-"+sliderItemWidth*slideNumber+"px"
+})
+
+//Prev Slider
+prevBtn.addEventListener('click',() => {
+  slideNumber--;
+  
+  if(slideNumber < 0){
+      slideNumber = numberOfSliderItems-1;
+  }
+
+  sliderMain.style.left = "-"+sliderItemWidth*slideNumber+"px"
+})
+
+//Auto Play
+var playSlider;
+var auto = () => {
+  playSlider = setInterval(() =>{
+      slideNumber++;
+      
+      if(slideNumber > (numberOfSliderItems-1) ){
+          slideNumber=0;
+      }
+  
+      sliderMain.style.left = "-"+sliderItemWidth*slideNumber+"px"
+  }, 5000)
+}
+auto()
+
 // auto increment counter
 const total = document.querySelector('#counter-total')
 const tour = document.querySelector('#counter-tour')
